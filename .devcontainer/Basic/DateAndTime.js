38016d98ee815988c455bcb53522791c68a1d914
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-06-26 21:30:54 +0000

    Date AND tIME

---
+++ .devcontainer/Basic/DateAndTime.js
@@ -0,0 +1,8 @@
+//****date and time in js  */
+ let mydate=new Date()
+ console.log(mydate)//give date and time
+ console.log(mydate.toString())//universal time
+ console.log(mydate.toDateString())
+ console.log(mydate.toLocaleString())
+ let mycreatdate= new Date(2023,3,3)
+ console.log(mycreatdate.toDateString())
---
+++ .devcontainer/Basic/Number.js
@@ -0,0 +1,20 @@
+// const hubdreds=10000000000
+// console.log(hubdreds.toLocaleString());
+// console.log(hubdreds.toLocaleString('en-IN'));
+// const Othernumber=123.7878
+// console.log(Othernumber.toPrecision(3))
+//***************************** */
+//   console.log(Math)
+//   console.log(Math.abs(-4))
+//   console.log(Math.round(4.6))
+//   console.log(Math.ceil(4.2))
+//   console.log(Math.floor(5.4))
+//   console.log(Math.abs(-4))
+//   console.log(Math.random())
+//   console.log(Math.random()*10+2)
+//math.random given the value between 0 to 1
+const min=10
+const max=20
+//console.log(Math.floor(Math.random()*(max-min+1))+min)
+//console.log(Math.random()*20+min)
+console.log(Math.random()*10+20)

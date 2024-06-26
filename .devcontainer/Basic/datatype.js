a373d2d61e0b1fe8fbd722c720f3f0d90467de18
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-06-26 16:48:19 +0000

    datatypes

---
+++ .devcontainer/Basic/datatype.js
@@ -0,0 +1,30 @@
+ "use strict";// trate all js code as newer version
+ //alert("shubham") we are using in nodejs,not browser
+console.log(3+4);
+console.log("shubham")
+let name ="shubhamchaurasiya"
+let age=18
+let isLoggedIn=false
+let state=null// it is a object type datatype
+let first;//undefine
+
+//Number-> range of number=>2 to 53
+//BigInt ----stock market treding
+//string "double qoutes",'single quotes'
+//boolEAN=> true / false 
+//null-> stand alon evalue(representtation on empty )
+//undefine -> decalre the value and but not initalize
+//symbol->unique data type
+//object=>
+    console.log(typeof("shubham"))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof("first"))
+console.log(typeof(first))
+
+
+
+
+
+
+

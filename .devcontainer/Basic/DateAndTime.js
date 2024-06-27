e65a2fb6c2b512200d23a28de9202f0584ee5c91
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-06-27 08:41:29 +0000

    switcj basic to advanced

--- .devcontainer/Basic/DateAndTime.js
+++ .devcontainer/Basic/DateAndTime.js
@@ -1,8 +1,16 @@
 //****date and time in js  */
- let mydate=new Date()
- console.log(mydate)//give date and time
- console.log(mydate.toString())//universal time
- console.log(mydate.toDateString())
- console.log(mydate.toLocaleString())
- let mycreatdate= new Date(2023,3,3)
- console.log(mycreatdate.toDateString())
+//  let mydate=new Date()
+//  console.log(mydate)//give date and time
+//  console.log(mydate.toString())//universal time
+//  console.log(mydate.toDateString())
+//  console.log(mydate.toLocaleString())
+//  let mycreatdate= new Date(2023,3,3)
+//  console.log(mycreatdate.toDateString())
+let newdate=new Date()
+console.log(newdate.getMonth())
+console.log(newdate.getDate())
+console.log(newdate.getDay())  
+console.log(newdate.toLocaleString('default',{
+    weekday:"long",
+  
+}))

ccac821a5a3d2399b1f371312c9bce4202e7fc8c
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-06-27 08:43:08 +0000

    variable

---
+++ .devcontainer/Basic/variable.js
@@ -0,0 +1,18 @@
+const accountId=10001;//lock the value//we can't change
+let accountEmail= "shubhamchaurasiyar98@gmail.com"  
+var accountPassword="shubham@123";
+ accountCity="bhopal"
+ let accountstate //undefined
+/*
+not use var 
+because of issue in block and functional scope
+
+
+
+*/
+// accountId=23;
+accountEmail="hav@gmail.com"
+accountPassword="1234"
+accountCity="punjav"
+ console.log(accountId)
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])

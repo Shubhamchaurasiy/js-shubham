983cf33d4817ca3f8f97270ec719ace620a5e59c
Shubhamchaurasiy <91664093+Shubhamchaurasiy@users.noreply.github.com>
2024-06-28 21:28:23 +0000

    28/16/2024 night 3.00

---
+++ Advanced/DOM.js
@@ -0,0 +1,9 @@
+//document.queryselector('h1')
+//document.queryselector('#title')
+//document.queryselector('.hrading')
+//document.querySelector('input[type="password"]')
+document.querySelector()
+const myh1= document.querySelectorAll('h1')
+myh1[0].style.color="yellow"
+//************************************************ */
+
